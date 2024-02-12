d75f17c78858c777e17d9e324acaecac60f78e82
joaoricardoM <joao_pia_mds@hotmail.com>
2024-02-12 12:36:26 -0300

    feat: add uppy

--- src/app/components/BatchUploadFiles.js
+++ src/app/components/BatchUploadFiles.js
@@ -1,55 +1,62 @@
-'use client'
-
+// components/BatchUploadFiles.js
 import React, { useState } from 'react'
 import axios from 'axios'
+import Uppy from '@uppy/core'
+import '@uppy/core/dist/style.css'
+import '@uppy/dashboard/dist/style.css'
+import { Dashboard } from '@uppy/react'
 
 const BatchUploadFiles = () => {
-  const [files, setFiles] = useState([])
+  const [fileStatus, setFileStatus] = useState('')
 
-  const handleFileChange = (e) => {
-    setFiles(e.target.files)
-  }
-
-  const handleUpload = async () => {
-    if (files.length === 0) {
-      alert('Por favor, selecione um ou mais arquivos.')
-      return
+  const uppy = new Uppy({
+    autoProceed: false,
+    restrictions: {
+      allowedFileTypes: ['.xml']
     }
+  })
 
+  uppy.on('complete', async (result) => {
     try {
       const formData = new FormData()
-      for (let i = 0; i < files.length; i++) {
-        formData.append('files', files[i])
-      }
+      result.successful.forEach((file) => {
+        formData.append('files', file.data)
+      })
       await axios.post('http://localhost:8080/files/lote', formData, {
         headers: {
           'Content-Type': 'multipart/form-data'
         }
       })
-      alert('Arquivos enviados com sucesso!')
+      setFileStatus('Arquivos enviados com sucesso!')
     } catch (error) {
       console.error('Erro ao enviar arquivos:', error)
-      alert('Erro ao enviar arquivos!')
+      setFileStatus('Erro ao enviar arquivos!')
     }
-  }
+  })
 
   return (
-    <div className="mb-4 flex items-center">
-      <label htmlFor="upload-input" className="cursor-pointer mr-4">
-        <input
-          type="file"
-          accept=".xml"
-          multiple
-          onChange={handleFileChange}
-          className="hidden"
-        />
-        <div className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded inline-block">
-          Upload em lote
-        </div>
-      </label>
+    <div className="mb-4">
+      <Dashboard
+        uppy={uppy}
+        inline
+        height={80}
+        width="100%"
+        proudlyDisplayPoweredByUppy={false}
+        locale={{
+          strings: {
+            dropPasteFiles: 'Solte arquivos aqui ou %{browseFiles}',
+            browseFiles: 'navegue'
+          }
+        }}
+      />
+      <p
+        style={{ font: 'small-caption', color: 'orange', marginBottom: '10px' }}
+      >
+        Somente upload de arquivos XML
+      </p>
       <button
         className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded"
-        onClick={handleUpload}
+        onClick={() => uppy.upload()}
       >
         Enviar
       </button>
